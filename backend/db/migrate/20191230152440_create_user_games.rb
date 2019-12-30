c3785db29a81f8f69bdb7464472a33900922269f
github username <github email address>
2019-12-30 15:25:11 +0000

    added models

---
+++ backend/db/migrate/20191230152440_create_user_games.rb
@@ -0,0 +1,11 @@
+class CreateUserGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_games do |t|
+      t.integer :user_id
+      t.integer :game_id
+      t.boolean :winner
+
+      t.timestamps
+    end
+  end
+end
