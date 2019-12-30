9cbcc2b11b4b2c743b6fb6973a6b097413c2c72d
github username <github email address>
2019-12-30 16:49:58 +0000

    added basic board

--- frontend/grid.js
+++ frontend/grid.js
@@ -4,9 +4,22 @@ function drawBoard() {
     
     for(let i = 0; i < 225; i++) {
         const tile = document.createElement('div');
-        //tile.classList.add("tile");
-        tile.classList.add("grid-item");
-        tile.innerText = i;
+
+        
+        tile.classList.add("empty");
+        tile.dataset.rowId = Math.floor(i/15) + 1;
+        tile.dataset.columnId = i%15 + 1;
+
+        const r = tile.dataset.rowId;
+        const c = tile.dataset.columnId;
+
+        if((r == 1 || r == 8 || r == 15) && (c == 1 || c == 8 || c == 15) && !(c == 8 && r == 8)) { //triple word tiles
+            tile.classList.add("triple-word");
+            tile.innerText = "TRIPLE WORD"
+        } else {
+            tile.classList.add("standard-tile");
+        }
+
         board.append(tile);
     }
     document.querySelector('body').append(board);
