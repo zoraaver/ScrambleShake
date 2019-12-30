1aa081c329c11125e22eead9362abf84103e2b7b
github username <github email address>
2019-12-30 15:52:21 +0000

    added json stuff

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
 
     def show
         user = User.find(params[:id])
-        render json: user
+
+        render json: user.to_json(include: {
+            games: {except: [:created_at, :updated_at]},
+            user_games: {only: :winner}
+        }, except: [:created_at, :updated_at])
     end
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -11,3 +11,23 @@ require 'faker'
     User.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email)
 end
 
+5.times do 
+    Game.create(score: rand(200))
+end
+
+
+UserGame.create(user_id: 1, game_id: 1, winner: true)
+UserGame.create(user_id: 5, game_id: 1, winner: false)
+UserGame.create(user_id: 3, game_id: 2, winner: true)
+UserGame.create(user_id: 7, game_id: 2, winner: false)
+UserGame.create(user_id: 6, game_id: 3, winner: true)
+UserGame.create(user_id: 10, game_id: 3, winner: false)
+UserGame.create(user_id: 9, game_id: 4, winner: true)
+UserGame.create(user_id: 2, game_id: 4, winner: false)
+UserGame.create(user_id: 4, game_id: 5, winner: true)
+UserGame.create(user_id: 8, game_id: 5, winner: false)
+
+
+
+
+
