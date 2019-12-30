1b6c5917451e2b4bdc83b7290157659f3061a3fa
github username <github email address>
2019-12-30 15:31:00 +0000

    added seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do 
+    User.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email)
+end
