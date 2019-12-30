24f6004546723c6b2d9f0a8c660e43480b4815d0
github username <github email address>
2019-12-30 17:18:48 +0000

    working on board

--- frontend/grid.js
+++ frontend/grid.js
@@ -13,10 +13,20 @@ function drawBoard() {
         const r = tile.dataset.rowId;
         const c = tile.dataset.columnId;
 
-        if((r == 1 || r == 8 || r == 15) && (c == 1 || c == 8 || c == 15) && !(c == 8 && r == 8)) { //triple word tiles
+        if((r == 1 || r == 8 || r == 15) && (c == 1 || c == 8 || c == 15) && !(c == 8 && r == 8)) { // triple word tiles
             tile.classList.add("triple-word");
             tile.innerText = "TRIPLE WORD"
-        } else {
+        } else if ((r == 2 || r == 6 || r == 10 || r == 14) && (c == 2 || c == 6 || c == 10 || c == 14) && // triple letter tiles
+         !(r == 2 && c == 2) && !(r == 14 && c == 14) && !(r == 14 && c == 2) && !(r == 2 && c == 14))
+        {
+            tile.classList.add("triple-letter");
+            tile.innerText = "TRIPLE LETTER"
+        } else if (((r == c) || (r == 16 - c)) && (((r > 1 && r < 6) || (r > 10 && r < 15)) && ((c > 1 && c < 6) || (c > 10 && c < 15))))
+        {
+            tile.classList.add("double-word");
+            tile.innerText = "DOUBLE WORD"
+        }
+         else {
             tile.classList.add("standard-tile");
         }
 
