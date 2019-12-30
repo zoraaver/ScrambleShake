e349a36cc645a705980e4746709c55cfcd8f0a20
github username <github email address>
2019-12-30 16:23:34 +0000

    added styles.css

--- frontend/grid.js
+++ frontend/grid.js
@@ -1,3 +1,15 @@
 function drawBoard() {
+    const board = document.createElement('div');
+    board.id = "board";
     
+    for(let i = 0; i < 225; i++) {
+        const tile = document.createElement('div');
+        //tile.classList.add("tile");
+        tile.classList.add("grid-item");
+        tile.innerText = i;
+        board.append(tile);
+    }
+    document.querySelector('body').append(board);
 }   
+
+drawBoard();
