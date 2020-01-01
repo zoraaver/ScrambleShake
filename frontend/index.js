25da093e0d106d4ca9829aefebf71aeeb5182970
PianoFF <Fangfei1star@gmail.com>
2020-01-01 17:06:45 +0000

    set up display for user login

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,1 @@
 // grabbing user inputs and update the db
-let from = document.querySelector('sign-in');
-let gameBegins = form.querySelector('submit-user');
-
-gameBegins.addEventListener('click',createUsers);
-
-const createUsers = e => {
-    e.preventDefault(); 
-    
-};
--- frontend/login.js
+++ frontend/login.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
     
     function startGame(e) {
         e.preventDefault();
-        const form = document.querySelector("#sign-in-form");
-        form.classList.add("hidden");
+        const login = document.querySelector("#sign-in-form");
+        login.classList.add("hidden");
         startButton.classList.add("hidden");
         const gamePage = document.querySelector("#game-page");
         gamePage.classList.remove("hidden");
