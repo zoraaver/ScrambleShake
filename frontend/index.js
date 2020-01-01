7deee44f296bc971f5cb54b4fee0da6077b4d56c
PianoFF <Fangfei1star@gmail.com>
2020-01-01 15:36:11 +0000

    user-info working progress

---
+++ frontend/index.js
@@ -0,0 +1,10 @@
+// grabbing user inputs and update the db
+let from = document.querySelector('sign-in');
+let gameBegins = form.querySelector('submit-user');
+
+gameBegins.addEventListener('click',createUsers);
+
+const createUsers = e => {
+    e.preventDefault(); 
+    
+};
