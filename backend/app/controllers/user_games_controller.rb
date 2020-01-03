8d0b11f99f0af1bc91751f4402553fcd4dea57f9
github username <github email address>
2020-01-03 13:50:18 +0000

    done wohoo

--- backend/app/controllers/user_games_controller.rb
+++ backend/app/controllers/user_games_controller.rb
@@ -1,2 +1,12 @@
 class UserGamesController < ApplicationController
+
+    def create
+        user_game = UserGame.create(user_id: params[:user_id], score: params[:score], winner: params[:winner])
+        render json: user_game
+    end
+
+    def index
+        user_games = UserGame.all
+        render json: user_games
+    end
 end
